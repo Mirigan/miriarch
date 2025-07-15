93aa624fe503b8ef7edf0c12c27bc9d2230d89e8
MiRiGaN <hello@mirigan.me>
2025-07-15 21:41:22 +0200

    fix font isntall

--- install/fonts.sh
+++ install/fonts.sh
@@ -1,3 +1,3 @@
-sudo pacman -Sy --noconfirm --needed \
+sudo pacman -S --noconfirm --needed \
   ttf-font-awesome ttf-cascadia-code-nerd ttf-cascadia-mono-nerd \
   noto-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts-extra
